60a15294bd85408c6d922b66cc72059c058d2804
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-10-17 23:05:15 -0300

    styles adjustment

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+function incrementStats(){
+  const counters = document.querySelectorAll('.stat')
+
+  counters.forEach((counter) => {
+    counter.innerText = 0
+    const updateCounter = () => {
+      const target = Number(counter.getAttribute('data-target'))
+      const insideContent = Number(counter.innerText)
+
+      const increment = target / 300
+      if(insideContent < target){
+        counter.innerText = Math.ceil(insideContent + increment)
+        setTimeout(updateCounter, 1)
+      }else{
+        counter.innerText = target
+      }
+    }
+    updateCounter()
+  })
+}
+document.addEventListener('DOMContentLoaded', incrementStats)
